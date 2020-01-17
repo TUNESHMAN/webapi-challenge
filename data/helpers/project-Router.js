e86d37171f80458aa07d51c43793efe8c3694e54
Babatunde <babatundea15@gmail.com>
2020-01-17 09:26:32 +0100

    I created end point for adding project

--- data/helpers/project-Router.js
+++ data/helpers/project-Router.js
@@ -2,4 +2,18 @@ const express = require(`express`);
 const Projects = require(`./projectModel`);
 const router = express.Router();
 
+router.post(`/`, () => {
+  Projects.insert(req.body)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(error => {
+      // I logged error to the server
+      console.log(error);
+      res.status(500).json({
+        message: `Error adding the project`
+      });
+    });
+});
+
 module.exports = router;
