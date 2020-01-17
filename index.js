f71d2e76320e538c6dead46ab9b640cb3d880a95
Babatunde <babatundea15@gmail.com>
2020-01-17 08:10:12 +0100

    I answered some self-study questions

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Here, i imported the server and listened to it.
 const server = require("./server");
 
-server.listen(6000, () => {
+server.listen(3000, () => {
   // Log a message on the server
-  console.log(`Running on port 6000`);
+  console.log(`Running on port 3000`);
 });
---
+++ server.js
@@ -0,0 +1,21 @@
+// I imported all the dependencies to the server file
+const express = require(`express`);
+const helmet = require(`helmet`);
+const actionRouter = require(`./data/helpers/action-Router`);
+const projectRouter = require(`./data/helpers/project-Router`);
+const server = express();
+
+server.use(express.json());
+server.use(helmet(/*The configuration of helmet will be done here*/));
+server.use(`/api/project`, projectRouter);
+server.get(`/`, logger, (req, res) => {
+  res.send(`<h2>Lambda Projects!</h2>`);
+});
+
+// I created a logger middleware
+function logger(req, res, next) {
+  const timeStamp = Date.now();
+  console.log(req.method, timeStamp, req.url);
+}
+// I exported the server
+module.exports = server;
