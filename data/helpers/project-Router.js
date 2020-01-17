429c85ceab3ac62624a0ef017a6f91c89b37cfb6
Babatunde <babatundea15@gmail.com>
2020-01-17 10:17:49 +0100

    I implemented the delete project endpoint

--- data/helpers/project-Router.js
+++ data/helpers/project-Router.js
@@ -2,6 +2,7 @@ const express = require(`express`);
 const Projects = require(`./projectModel`);
 const router = express.Router();
 
+// ENDPOINT TO ADD A PROJECT
 router.post(`/`, () => {
   Projects.insert(req.body)
     .then(data => {
@@ -16,6 +17,7 @@ router.post(`/`, () => {
     });
 });
 
+// ENDPOINT TO ADD A PROJECT BY ID
 router.post(`/:id/project`, (req, res) => {
   projectInfo = { ...req.body, project_id: req.params.id };
   Projects.insert(projectInfo)
@@ -27,6 +29,41 @@ router.post(`/:id/project`, (req, res) => {
       console.log(error);
       res.status(500).json({
         message: `There was an error handling this project`
+      });
+    });
+});
+// ENDPOINT TO GET A PROJECT
+router.get(`/`, (req, res) => {
+  Projects.get()
+    .then(project => {
+      res.status(200).json(project);
+    })
+    .catch(error => {
+      // Error is logged to the server
+      console.log(error);
+      error.status(500).json({
+        message: `Error retrieving the project`
+      });
+    });
+});
+
+// DELETE A PROJECT BY ID
+router.delete(`/:id`, (req, res) => {
+  Projects.remove(req.params.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({
+          message: `Project deleted from database`
+        });
+      } else {
+        message: `Error finding the project`;
+      }
+    })
+    .catch(error => {
+      // Log error on server
+      console.log(error);
+      res.status(500).json({
+        message: `Project cannot be found`
       });
     });
 });
